4f4ef65d63f8a9d69a3b2967554eee3023a126bd
nicholas-s-perkins <nsperkins@spscommerce.com>
2025-03-17 12:47:45 -0500

    Added initial code.

---
+++ demo-intellij-maven-generated-sources-loading/src/main/java/org/example/DemoRecord.java
@@ -0,0 +1,7 @@
+package org.example;
+
+
+import io.soabase.recordbuilder.core.RecordBuilder;
+
+@RecordBuilder
+public record DemoRecord(String prop1) {}
---
+++ demo-intellij-maven-generated-sources-loading/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DemoRecordBuilder.builder().prop1("val1").build());
+    }
+}
